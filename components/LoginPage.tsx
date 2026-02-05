c913a0235bf5614e78bdeb09174692ec133c051d
sawyer-gif <sawyer@marioromano.com>
2026-02-05 11:34:57 -0500

    Update LoginPage.tsx

--- components/LoginPage.tsx
+++ components/LoginPage.tsx
@@ -18,7 +18,7 @@ export const LoginPage: React.FC = () => {
   loop
   playsInline
   className="absolute inset-0 w-full h-full object-cover opacity-70"
-  poster="https://images.unsplash.com/photo-1550684848-fac1c5b4e853?auto=format&fit=crop&q=80&w=2000"
+
 >
   <source src="/video/login-bg.mp4" type="video/mp4" />
 </video>
